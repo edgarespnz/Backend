e3b74172d807597742b25319a8e500cfd2379536
edgarespnz <20163070@aloe.ulima.edu.pe>
2022-11-28 02:30:09 -0500

    endpoints

--- dao/index.js
+++ dao/index.js
@@ -1,18 +1,18 @@
-const {Sequelize, DataTypes} = require("sequelize");
+const { Sequelize, DataTypes, UUIDV4 } = require("sequelize");
 
-const CADENA_CONEXION = 
-"postgresql://postgres:postgres@localhost:5432/product_backlog"
+const CADENA_CONEXION =
+    "postgresql://postgres:postgres@localhost:5432/product_backlog"
 
 const sequelize = new Sequelize(CADENA_CONEXION)
 
 //para las columnas createdAt y updatedAt
 const timestamps = {
-    timestamps : false,
+    timestamps: false,
     freezeTableName: true
 }
 
-//alias de la tabla y de qué tabla se referencia
-const Productos = sequelize.define("producto",{
+//tabla productos
+const Productos = sequelize.define("producto", {
     Producto_ID: {
         primaryKey: true,
         type: DataTypes.UUID,
@@ -21,54 +21,60 @@ const Productos = sequelize.define("producto",{
     },
     Nombre: {
         type: DataTypes.STRING(100),
-        allowNull : false
+        allowNull: false
     },
     Precio: {
         type: DataTypes.NUMBER(20),
         allowNull: false
     },
-    Descripcion : {
-        type : DataTypes.STRING(150),
+    Descripcion: {
+        type: DataTypes.STRING(150),
         allowNull: true
     },
     Categoria: {
-        type : DataTypes.STRING(100),
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    Url: {
+        type: DataTypes.STRING(200),
         allowNull: true
     }
-},timestamps)
+}, timestamps)
 
-const Usuarios = sequelize.define("usuario",{
+
+//tabla usuarios
+const Usuarios = sequelize.define("usuario", {
     Usuario_ID: {
         primaryKey: true,
         type: DataTypes.UUID,
         defaultValue: Sequelize.UUIDV4,
-        allowNull : false
+        allowNull: false
     },
     Nombre: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    Apellido:{
+    Apellido: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    Correo:{
+    Correo: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    Contraseña:{
+    Contraseña: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    Direccion:{
+    Direccion: {
         type: DataTypes.STRING(100),
         allowNull: true
     },
-    Departamento:{
+    Departamento: {
         type: DataTypes.STRING(100),
         allowNull: true
     },
-    Ciudad:{
+    Ciudad: {
         type: DataTypes.STRING(100),
         allowNull: true
     },
@@ -76,15 +82,244 @@ const Usuarios = sequelize.define("usuario",{
         type: DataTypes.STRING(100),
         allowNull: true
     },
-    Telefono : {
+    Telefono: {
         type: DataTypes.NUMBER(100),
+        allowNull: true
+    }
+}, timestamps)
+
+//tabla orden
+const Orden = sequelize.define("orden", {
+    Orden_ID: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: sequelize.UUIDV4,
+        allowNull: false
+    },
+
+    Usuario_ID: {
+        type: DataTypes.UUID,
+        allowNull: false
+
+    },
+    Monto: {
+        type: DataTypes.FLOAT(2),
+        allowNull: true
+    },
+    Direccion: {
+        type: DataTypes.STRING(200),
+        allowNull: true
+    },
+    Fecha: {
+        type: DataTypes.DATE(),
+        allowNull: false,
+    }
+
+}, timestamps)
+
+//tabla orden_producto
+const Orden_Producto = sequelize.define("orden_producto", {
+    Orden_Producto_ID: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+
+    Orden_ID: {
+        type : DataTypes.UUID,
+        allowNull: true
+    },
+    Producto_ID :{
+        type: DataTypes.UUID,
+        allowNull: true
+    }
+},timestamps)
+
+//tabla pc armado producto
+const PC_Armado_Producto = sequelize.define("pc_armado_producto", {
+    PC_Armado_Producto_ID : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        allowNull : false,
+        defaultValue : sequelize.UUIDV4
+    },
+    PC_Armado_ID : {
+        type : DataTypes.UUID,
+        allowNull : true
+    },
+    Producto_ID : {
+        type : DataTypes.UUID,
         allowNull : true
     }
 },timestamps)
- 
+
+//tabla pc armado
+const PC_Armado = sequelize.define("pc_armado", {
+    PC_Armado_ID : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        allowNull : false,
+        defaultValue : sequelize.UUIDV4
+    },
+    Nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    },
+    Descripcion :{
+        type : DataTypes.STRING(200),
+        allowNull : true
+    }
+}, timestamps)
+
+//tabla reseña
+const Resena = sequelize.define("resena",{
+    Resena_ID : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        allowNull : false,
+        defaultValue : sequelize.UUIDV4
+    },
+    Usuario_ID : {
+        type : DataTypes.UUID,
+        allowNull : false,
+    },
+    Puntaje : {
+        type : DataTypes.NUMBER(),
+        allowNull : true,
+    },
+    Comentario : {
+        type : DataTypes.STRING(),
+        allowNull : true
+    },
+    Link : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    },
+    Tipo_Resena : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    }
+},timestamps)
+
+//tabla reporte
+const Reporte = sequelize.define("reporte", {
+    Reporte_ID : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : sequelize.UUIDV4,
+        allowNull : false
+    },
+    Usuario_ID : {
+        type : DataTypes.UUID,
+        allowNull: true
+    },
+    Correo : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    },
+    Nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    },
+    Telefono : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    },
+    Asunto : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    },
+    Descripcion : {
+        type : DataTypes.STRING(200),
+        allowNull : true
+    }
+},timestamps)
 
 //relaciones entre tablas
+
+// Reporte * <----> 1 Usuario
+Reporte.belongsTo(Usuarios, {
+    foreignKey : "Usuario_ID"
+})
+Usuarios.hasMany(Reporte, {
+    foreignKey : "Reporte_ID"
+})
+
+
+// Reseña * <----> 1 Usuario
+Resena.belongsTo(Usuarios, {
+    foreignKey : "Usuario_ID"
+})
+
+Usuarios.hasMany(Resena, {
+    foreignKey : "Resena_ID"
+})
+
+// Reporte * <----> 1 Usuario
+Reporte.belongsTo(Usuarios, {
+    foreignKey: "Usuario_ID"
+})
+
+Usuarios.hasMany(Reporte, {
+    foreignKey: "Usuario_ID"
+})
+
+
+// Orden * <----> 1 Usuario
+Orden.belongsTo(Usuarios, {
+    foreignKey : "Usuario_ID"
+})
+
+Usuarios.hasMany(Orden, {
+    foreignKey : "Orden_ID"
+})
+
+
+// Orden_producto * <----> 1 Orden
+Orden_Producto.belongsTo(Orden, {
+    foreignKey : "Orden_ID"
+})
+
+Orden.hasMany(Orden_Producto, {
+    foreignKey : "Orden_ID"
+})
+
+// Producto 1 <----> * Orden_Producto
+Orden_Producto.belongsTo(Productos, {
+    foreignKey : "Producto_ID"
+})
+
+Orden_Producto.hasMany(Orden_Producto, {
+    foreignKey : "Producto_ID"
+})
+
+// PC_Armado_Producto * <----> 1 Productos
+PC_Armado_Producto.belongsTo(Productos,{
+    foreignKey:"Producto_ID"
+})
+Productos.hasMany(PC_Armado_Producto,{
+    foreignKey:"Producto_ID"
+})
+
+
+// PC_Armado_Producto * <----> 1 PC_Armado_Producto
+PC_Armado_Producto.belongsTo(PC_Armado,{
+    foreignKey:"PC_Armado_ID"
+})
+PC_Armado.hasMany(PC_Armado_Producto,{
+    foreignKey:"PC_Armado_ID"
+})
+
+
+//exportacion de las tablas
+
 module.exports = {
     Productos,
-    Usuarios
+    Usuarios,
+    Orden,
+    Orden_Producto,
+    PC_Armado_Producto,
+    PC_Armado,
+    Reporte,
+    Resena
 }
--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors")
 var crypto = require("crypto")
 
-const {Productos, Usuarios} = require("./dao")
+const { Productos,
+    Usuarios,
+    Orden,
+    Orden_Producto,
+    PC_Armado_Producto,
+    PC_Armado,
+    Reporte,
+    Resena } = require("./dao");
+const { Sequelize } = require("sequelize");
+const { stringify } = require("querystring");
 
 
 const app = express();
@@ -11,53 +20,204 @@ const PORT = 8888;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended : true
+    extended: true
 }))
 
 app.use(cors());
 
-app.get("/productos", async(req,resp)=>{
-    const listadoProductos = await Productos.findAll()
-    resp.send(listadoProductos)
+/*INICIO ENDPOINTS PARA OBTENER DATOS TOTALES*/
+//obtener todas las ordenes
+
+app.get("/ordenes", async (req, resp) => {
+    const listaOrdenes = await Orden.findAll()
+    resp.send(listaOrdenes)
+})
+
+app.get("/ordenes_productos", async (req, resp) => {
+
+    try {
+        const orden_id = req.query.orden
+        if (orden_id !== undefined) {
+            const listaProductoOrdenes = await Orden_Producto.findAll({
+                where: {
+                    Orden_ID: orden_id
+                }
+            })
+            resp.send(listaProductoOrdenes)
+        }
+
+    } catch (error) {
+        console.log("NO EXISTE LA ORDEN ASOCIADA")
+    }
 })
 
 
-//verificar que usuario existe por correo electronico
+/*select * from public."producto"
+inner join public."orden_producto"
+on public."producto"."Producto_ID" = public."orden_producto"."Producto_ID"*/
+app.get("/cart", async (req, resp) => {
+    try {
+        const orden_id = req.query.orden
+        const productos = await Orden_Producto.findAll({
+            include: Productos,
+            where: {
+                Orden_ID: orden_id
+            }
+        })
+        resp.send(productos)
 
-app.get("/usuarios", async(req,resp)=>{
+    } catch (error) {
+        resp.send("ERROR")
+    }
+})
+
+//obtener todas las pcs armadas
+app.get("/pc_armados", async (req, resp) => {
+    const listaPcArmados = await PC_Armado.findAll()
+    resp.send(listaPcArmados)
+})
+
+//obtener productos asociados a una pc armada
+app.get("/productos_pc_armados", async (req, resp) => {
+    const listaProductosPcArmados = await PC_Armado_Producto.findAll()
+    resp.send(listaProductosPcArmados)
+})
+
+//obtener todos los productos
+app.get("/productos", async (req, resp) => {
+    try {
+        const producto_id = req.query.id
+
+        if (producto_id === undefined) {
+            const listadoProductos = await Productos.findAll()
+            resp.send(listadoProductos)
+        }
+        else {
+            const productosFiltrados = await Productos.findAll({
+                where: {
+                    Producto_ID: producto_id
+                }
+            })
+            resp.send(productosFiltrados)
+        }
+    } catch (error) {
+        console.log("error en la consulta")
+    }
+
+
+})
+
+//obtener todos los reportes
+app.get("/reportes", async (req, resp) => {
+    const listadoReportes = await Reporte.findAll()
+    resp.send(listadoReportes)
+})
+
+//obtener todas las reseñas
+app.get("/resenas", async (req, resp) => {
+    const listaResenas = await Resena.findAll()
+    resp.send(listaResenas)
+})
+
+//obtener todos los usuarios
+app.get("/usuarios_all", async (req, resp) => {
     const listaUsuarios = await Usuarios.findAll()
     resp.send(listaUsuarios)
 })
 
-//crear nuevo usuario
-
-app.post("/create_user", async(req,resp)=>{
-    const option = req.query.opcion;
-    const nombre = req.query.nombre;
-    const correo = req.query.correo;
-    const contrasena = req.query.contrasena;
-    
-    const createuserid = crypto.randomUUID()
+//obtener usuario por correo
+app.get("/usuarios", async (req, resp) => {
     try {
-        if(option == "crear"){
-            await Usuario.create({
-                Usuario_ID : `${createuserid}`,
-                Nombre: nombre,
-                Apellido: apellido,
-                Correo: correo,
-                Contraseña : contrasena,
-                Direccion: "",
-                Departamento: "",
-                Ciudad: "",
-                Codigo_Postal : "",
-                Telefono: ""
-            })
-        }
+        const correo = req.query.correo
+        const listaUsuarios = await Usuarios.findAll({
+            where: {
+                Correo: correo
+            }
+        })
+        resp.send(listaUsuarios)
+
     } catch (error) {
-        
+        resp.send("error al obtener usuario")
     }
 })
 
-app.listen(PORT, ()=>{
+/*FIN DE ENDPOINTS PARA OBTENER DATOS TOTALES*/
+
+
+
+/*ENDPOINT PARA INSERTAR DATOS EN LAS TABLAS*/
+//crear nuevo usuario
+//recibir data en cuerpo de petición POST
+//request: 
+/*{
+    "firstName" : "nombreprueba",
+    "lastName" : "apellidoprueba",
+    "password" : "passwordprueba",
+    "email" : "emailprueba"
+}*/
+app.post("/create_user", async (req, resp) => {
+
+    const dataRequest = req.body
+    const firstName = dataRequest.firstName
+    const lastName = dataRequest.lastName
+    const email = dataRequest.email
+    const password = dataRequest.password
+
+    const createuserid = crypto.randomUUID()
+
+    if (firstName == null || firstName == undefined) {
+        resp.send({ error: "ERROR: Debe enviar un first name" })
+    }
+
+    if (lastName == null || lastName == undefined) {
+        resp.send({ error: "ERROR: Debe enviar un last Name" })
+    }
+
+    if (email == null || email == undefined) {
+        resp.send({ error: "ERROR: Debe enviar un email" })
+    }
+
+    if (password == null || password == undefined) {
+        resp.send({ error: "ERROR: Debe enviar un password" })
+    }
+
+    const listaUsuarios = await Usuarios.findAll({
+        where : {
+            Correo : email
+        }
+    })
+
+    if (listaUsuarios.length > 0) {
+        resp.send({
+           error: "EL CORREO YA EXISTE"
+        })
+        return
+    }
+    try {
+        await Usuarios.create({
+            Usuario_ID: createuserid,
+            Nombre: firstName,
+            Apellido: lastName,
+            Correo: email,
+            Contraseña: password,
+            Direccion: null,
+            Departamento: null,
+            Ciudad: null,
+            Codigo_Postal: null,
+            Telefono: null
+        })
+    } catch (error) {
+        resp.send({
+            error: `ERROR.${error}`
+        })
+        return
+    }
+    resp.send({
+        error: ""
+    })
+
+})
+
+app.listen(PORT, () => {
     console.log(`Servidor web iniciado en puerto: ${PORT}`)
 })
